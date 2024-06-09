f94080985e5eb13c9b10b7bb9e75ceaea1d75337
hamond12 <chicken010208@gmail.com>
2024-06-09 14:50:09 +0900

    계산기 클래스 작성

---
+++ app/src/main/java/com/example/calculator/calculator.kt
@@ -0,0 +1,77 @@
+class Calculator {
+    private fun opPriority(op: Char): Int {
+        return when (op) {
+            '+', '-' -> 1
+            '*', '/' -> 2
+            else -> 0
+        }
+    }
+    private fun infixToPostfix(str: String): String {
+        val num = StringBuilder()
+        val stack = mutableListOf<Char>()
+
+        for (i in str) {
+            when {
+                i.isDigit() || i == '.' -> num.append("$i")
+                i == '(' -> stack.add(i)
+                i == ')' -> {
+                    while (stack.isNotEmpty() && stack.last() != '(') {
+                        num.append(" ${stack.removeAt(stack.lastIndex)}")
+                    }
+                    stack.removeAt(stack.lastIndex)
+                }
+                i in "+-*/" -> {
+                    while (stack.isNotEmpty() && opPriority(i) <= opPriority(stack.last())) {
+                        num.append(" ${stack.removeAt(stack.lastIndex)}")
+                    }
+                    stack.add(i)
+                    num.append(" ")
+                }
+            }
+        }
+
+        while (stack.isNotEmpty()) {
+            num.append(" ${stack.removeAt(stack.lastIndex)}")
+        }
+
+        return num.toString().trim()
+    }
+
+    private fun calculatePostfix(postfixEx: String): Double {
+        val stack = mutableListOf<Double>()
+
+        for (i in postfixEx.split(" ")) {
+            // 피연산자인 경우 스택에 추가
+            if (i.matches(Regex("-?\\d+(\\.\\d+)?"))) stack.add(i.toDouble())
+
+            // 연산자인 경우 스택에서 피연산자들을 꺼내서 계산하고 결과를 다시 스택에 추가
+            else {
+                val op1 = stack.removeAt(stack.lastIndex)
+                val op2 = stack.removeAt(stack.lastIndex)
+                when (i) {
+                    "+" -> stack.add(op1 + op2)
+                    "-" -> stack.add(op1 - op2)
+                    "*" -> stack.add(op1 * op2)
+                    "/" -> stack.add(op1 / op2)
+                }
+            }
+        }
+
+        return stack.first()
+    }
+    fun calculate(ex: String): Any {
+        val postfix = infixToPostfix(ex)
+        val res = calculatePostfix(postfix)
+        return if (res % 1.0 == 0.0) res.toInt() else res
+    }
+}
+
+fun main() {
+    val calculator = Calculator()
+
+    print("수식을 입력하세요: ")
+    val input = readln()
+
+    val result = calculator.calculate(input)
+    println("계산 결과: $result")
+}
